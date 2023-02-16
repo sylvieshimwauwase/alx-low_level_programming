fb8a33d01a4bf030255bfa7ca9f9ba145a554717
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-16 07:55:41 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-int main()
+/**
+ * main - program to print using puts function
+ * Return:0 -succeed
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
