a778fe39e127224a5851196c6c23ed7c6a37ae8e
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-16 10:06:29 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - a program to print the message using printf
+ * Return: 0 succeed
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
