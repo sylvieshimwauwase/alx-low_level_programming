abfb6211d1b7b0762307e4085531e0cd1a3be079
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-16 15:22:17 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include<unistd.h>
+
 /**
  * main -a program that prints a message
- * Return:1 succeed
+ *
+ * Return: Always 1 (succeed)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\"- Dora Korpar, 2015 -10 -19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015 -10 -19\n", 59);
+	return (1);
 }
-
