ca1ef652bdf3e2a4c77edd98f07e3530b30c4af1
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-18 12:47:55 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program to print alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		puchar(alp);
+		alp++;
+	}
+	return (0);
+}
