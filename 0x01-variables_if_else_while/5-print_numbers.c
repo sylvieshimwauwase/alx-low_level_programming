764990c60c3c74284de1b0ae70bdd14158d5409e
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-18 13:55:53 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main - program to print single numbers of base 10 from 0
+ *
+ *  Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
