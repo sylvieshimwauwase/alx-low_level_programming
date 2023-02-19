d8dfab8c5f00d7273a19144b0fd3bf88c4ef1ad7
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-18 16:00:58 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (n1 = 0; n1 < 10; n1++)
 	{
-		for (n2 = n1+1; n2 < 10; n2++)
+		for (n2 = n1 + 1; n2 < 10; n2++)
 		{
 			putchar((n1 % 10) + '0');
 			putchar((n2 % 10) + '0');
 
-			if (n1 == 9 && n2 == 9)
+			if (n1 == 8 && n2 == 9)
 				continue;
 
 			putchar(',');
