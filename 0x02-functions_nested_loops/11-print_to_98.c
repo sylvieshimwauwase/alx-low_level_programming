0ee1b62d11771d95199aee8755eb5719eb005ce6
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-21 14:04:13 -0800

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,7 @@
  * print_to_98 - prints all natural numbers from n to 98
  * @n: number
  *
- * Return: always number
+ * Return: always 0
  */
 void print_to_98(int n)
 {
@@ -15,13 +15,13 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf ("%d", n);
-				printf ("\n");
+				printf("%d", n);
+				printf("\n");
 				break;
 			}
 			else
 			{
-				printf ("%d,", n);
+				printf("%d,", n);
 			}
 		}
 	}
@@ -31,13 +31,13 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf ("%d", n);
-				printf ("%d", n);
+				printf("%d", n);
+				printf("%d", n);
 				break;
 			}
 			else
 			{
-				printf ("%d,", n);
+				printf("%d,", n);
 			}
 		}
 	}
