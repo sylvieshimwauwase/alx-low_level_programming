bda381857a527c47bf7830f70053e424dca3c8dc
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-21 02:34:23 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,6 @@
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	write("_putchar\n");
 	return (0);
 }
