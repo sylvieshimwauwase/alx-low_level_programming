89777d896ffa23b6a1e1e6ec1de6ea5af175a1ff
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-21 10:32:54 -0800

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,12 @@
 /**
  * _islower - lowercase letter
  *
+ * @c: character
  * Return: 1 for lowercase character or 0 for anything else
  */
 int _islower(int c)
 {
-	if (c >= 1 && c <= 26)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
