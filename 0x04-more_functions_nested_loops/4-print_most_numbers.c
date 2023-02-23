3fbca6d61678ac573f5f28daebbd647d1f5c266f
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-23 14:38:54 -0800

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 
 	for (c = 0; c < 10; c++)
 	{
-		if ((c == 2) || (c == 4))
+		if (c == 2 || c == 4)
 		{
 			continue;
 		}
@@ -20,5 +20,4 @@ void print_most_numbers(void)
 		}
 	}
 		_putchar('\n');
-}
-	
+}	
