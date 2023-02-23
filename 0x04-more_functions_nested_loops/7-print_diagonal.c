9c859b836e29b4c5b1f94218906b2be5f4d7c56a
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-23 12:17:34 -0800

    7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal in terminal
+ * @n: number of diagonal
+ * Return: diagonal
+ */
+void print_diagonal(int n)
+{
+	int c, d;
+
+	if (n <=0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (c = 0; c <= n; c++)
+		{
+			for (d =0; d < c; d++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
