f60cb8189d1fccd8b2073106c6bd56e43eade401
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-28 10:01:46 -0800

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,28 +5,35 @@
  */
 int _atoi(char *s)
 {
-	int res = 0;
-	int sign = 1;
 	int i = 0;
+	int d = 0;
+	int n = 0;
+	int len = 0;
+	int f = 0;
+	int digit = 0;
 
-	while (s[i] == ' ')
-		i++;
+	while (s[len] != '\0')
+		len++;
 
-	if (s[i] == '-')
+	while (f < len && f == 0)
 	{
-		sign = -1;
+		if (s[i] == '-')
+			d++;
+		if (s[i] >= '0' && s[i] < 10)
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
 		i++;
 	}
-	else if (s[i]  == '+')
-	{
-		i++;
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		res = res * 10 + (s[i] - '0');
-		i++;
-	}
-	return (sign * res);
+	if (f == 0)
+		return (0);
+	return (n);
 }
 
