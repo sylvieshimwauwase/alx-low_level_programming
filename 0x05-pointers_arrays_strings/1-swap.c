1262a30bd6cc7539d5a6c8c7bd1bb414d1d49dcc
sylvieshimwauwase <slvshimwa@gmail.com>
2023-02-28 04:17:03 -0800

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * swap_int - swap integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: always 0
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
