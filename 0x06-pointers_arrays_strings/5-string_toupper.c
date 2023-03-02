e2fa58cea6b54cb11ef58c52cbc4988ea024e428
sylvieshimwauwase <slvshimwa@gmail.com>
2023-03-02 10:45:56 -0800

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,13 +7,14 @@
  */
 char *string_toupper(char *str)
 {
-	int a = 0;
+	int a;
 
+	a = 0;
 	while (str[a] != '\0')
 	{
 		if (str[a] >= 'a' && str[a] <= 'z')
 			str[a] = str[a] - 32;
 		a++;
 	}
-	return (str)
+	return (str);
 }
