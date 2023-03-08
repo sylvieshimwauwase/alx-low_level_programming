2fe4fa3c10525321a5a445b4b181bf607228bc25
sylvieshimwauwase <slvshimwa@gmail.com>
2023-03-08 13:28:50 +0200

    3-factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - return factorial of a number
+ * @n:number
+ * Return:number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
