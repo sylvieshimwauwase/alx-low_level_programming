bd054e411efdb8a71a74ffef337f4130b6f7dce9
sylvieshimwauwase <slvshimwa@gmail.com>
2023-03-09 17:31:12 +0200

    100-is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * is_palindrome - returns a palindrome
+ * _strlen_recursion - returns length of a string
  * @s:string
- * Return:palindrome string
+ * Return:length
  */
 int _strlen_recursion(char *s)
 {
-	if (*s =='\0')
+	if (*s == '\0')
 	{
 		return (0);
 	}
@@ -18,31 +18,31 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * _strlen_recursion - returns length of a string
- * @s:string
- * Return:length
- */
-int comp(char *s, int i, int j)
-{
-	if (*(s + i) == *(s +j))
-	{
-		if (i == j || i == j + 1)
-			return (1);
-		return (0 + comp(s, i + 1,j -1));
-	}
-	return (0);
-}
-
-/**
  * comp - compares string
  * @s:string
  * @i:iteration
  * @j:iteration
  * Return:string
  */
+int comp(char *s, int i, int j)
+{
+	if (*(s + i) == *(s + j))
+	{
+		if (i == j || i == j + 1)
+			return (1);
+		return (0 + comp(s, i + 1, j - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - returns a palindrome
+ * @s:string
+ * Return:palindrome string
+ */
 int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (comp(s, 0,_strlen_recursion(s) -1));
+	return (comp(s, 0, _strlen_recursion(s) - 1));
 }
