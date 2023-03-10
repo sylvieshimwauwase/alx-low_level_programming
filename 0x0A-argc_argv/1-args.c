aa2796c1af85f7c0d0eacd81884012c2b713d086
sylvieshimwauwase <slvshimwa@gmail.com>
2023-03-10 19:16:19 +0200

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints number of arguments
+ * @argc:number of arguments
+ * @argv:array of arguments
+ * Return:always 0
+ */
+int main (int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
