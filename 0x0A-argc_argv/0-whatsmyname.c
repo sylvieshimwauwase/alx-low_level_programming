f07d2cb90bec0171d77d159b002ddb900450b6ce
sylvieshimwauwase <slvshimwa@gmail.com>
2023-03-10 19:07:41 +0200

    0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,8 +3,8 @@
 
 /**
  * main - prints the name of the program
- * argc:number of arguments
- * argv:array of arguments
+ * @argc:number of arguments
+ * @argv:array of arguments
  * Return: always 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
