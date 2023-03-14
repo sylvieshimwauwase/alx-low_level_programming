bcef71a67ac63e8a05c25b3938134f64f396dfeb
sylvieshimwauwase <slvshimwa@gmail.com>
2023-03-14 16:35:23 +0200

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,26 +13,27 @@ char *str_concat(char *s1, char *s2)
 	char *conc_str;
 	int q;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
 	}
-	else if (s1 == NULL)
+	if (s2 == NULL)
 	{
-		return (strdup(s2));
-	}
-	else if (s2 == NULL)
-	{
-		return (strdup(s1));
+		s2 = "";
 	}
 	q = strlen(s1) + strlen(s2);
-	conc_str = (char *)malloc((q + 1) * sizeof(char));
+
+	conc_str = malloc(q + 1);
 
 	if (conc_str == NULL)
 	{
 		return (NULL);
 	}
 	strcpy(conc_str, s1);
-	strcat(conc_str, s2);
+	strcpy(conc_str + strlen(s1), s2);
+	conc_str[q] = '\0';
+
 	return (conc_str);
 }
+
+
