a21dba85ba310bd9bc77cc43d355c7387e3688f2
sylvieshimwauwase <slvshimwa@gmail.com>
2023-03-20 15:53:15 +0200

    defining new type

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * struct dog - dog's style
+ * struct dog - dog's new style
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner:owner of the dog
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog's new type
+ * @name: name of the dog
+ * @age:age of the dog
+ * @owner:owner of the dog
+ */
 struct dog
 {
 	char *name;
