f67afdbe5dc460c0203207ee00bc2413a0415f6a
sylvieshimwauwase <slvshimwa@gmail.com>
2023-03-20 12:33:31 +0200

    function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+/** main - function that computes absolute value
+ * Return:always 0 on success
+ */
+int main(void)
+{
+	printf("the absolute value of %d is %d", x, ABS(x));
+	return (0);
+}
