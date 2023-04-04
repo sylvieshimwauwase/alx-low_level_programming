1555667a5d928bba301d1115715a16222a836a88
sylvieshimwauwase <slvshimwa@gmail.com>
2023-04-04 12:03:08 +0200

    adding new node at any index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,12 +23,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
 	current_node = *head;
 	for (i = 0; i < idx - 1; i++)
 	{
 		current_node = current_node->next;
 	}
-	new_node->n = n;
+	if (current_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	new_node->next = current_node->next;
 	current_node->next = new_node;
 
