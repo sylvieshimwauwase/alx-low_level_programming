598e5425d96251579a809acbe37926a64294262e
sylvieshimwauwase <slvshimwa@gmail.com>
2023-04-06 14:16:02 +0200

    binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		a = a * 2 +(b[i] == '1' ? 1 : 0);
+		a = a * 2 + (b[i] == '1' ? 1 : 0);
 	}
 	return (a);
 }
