df5eaf8641673168f0e46b2e744e55946a5e4945
sylvieshimwauwase <slvshimwa@gmail.com>
2023-04-11 12:15:20 +0200

    copies from one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: file_from file_to\n");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
