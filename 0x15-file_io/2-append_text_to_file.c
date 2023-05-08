7686049efbf20a1e06b3043ccc78c25aab1dfae9
sylvieshimwauwase <slvshimwa@gmail.com>
2023-05-08 13:12:17 +0200

    append text at end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,10 +13,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	FILE *file = fopen(filename, "a");
 
 	if (filename == NULL)
-                return (-1);
+		return (-1);
 
 	if (file == NULL)
-		return(-1);
+		return (-1);
 
 	if (text_content != NULL)
 	{
