9f0d77eb823f7dc8317157156881ecd7d2f02edf
sylvieshimwauwase <slvshimwa@gmail.com>
2023-05-08 12:44:43 +0200

    function that creates file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: text to write
+ *
+ * ReturnL 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, len;
+	ssize_t write_bytes;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR |  S_IWUSR);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		write_bytes = write(file, text_content, len);
+		if (write_bytes != len)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
+}
