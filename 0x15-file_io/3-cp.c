461ec7655e8971fab883f075754e9d35f5839a38
sylvieshimwauwase <slvshimwa@gmail.com>
2023-05-08 20:24:19 +0200

    copies content of file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,14 +16,13 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to;
-	ssize_t read_bytes, write_bytes;
+	int file_from, file_to, read_bytes, write_bytes;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
@@ -56,10 +55,5 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", errno);
 		exit(100);
 	}
-	/**if (close(file_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}*/
 	return (0);
 }
