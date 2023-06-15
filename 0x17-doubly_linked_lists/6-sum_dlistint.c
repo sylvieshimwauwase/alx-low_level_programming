eea89bf7c58f6c1465bd8d7ab9ef75145de5ca3e
sylvieshimwauwase <slvshimwa@gmail.com>
2023-06-15 19:23:38 +0200

    sum of linked lists data

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum of the data in linked list
+ * @head: head of the linked list
+ *
+ * Return: sum of the data 
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	int sum = 0;
+
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+
+	return (sum);
+}
