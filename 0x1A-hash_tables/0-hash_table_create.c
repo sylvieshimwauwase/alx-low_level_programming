ec16d5bf1c38795433aa3c0117419adb9defd135
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-11 21:11:17 +0200

    Hash Table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,13 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
+	unsigned long int i;
+	hash_table_t *hash_table = NULL;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
@@ -18,11 +24,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table->size = size;
 
-	hash_table->array = malloc(size * sizeof(int));
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
 	}
 
 	return (hash_table);
