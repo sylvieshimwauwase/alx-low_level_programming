7c12b7182e72ad49e3c8335aa5ed04f69e468051
sylvieshimwauwase <slvshimwa@gmail.com>
2023-10-10 13:11:04 +0200

    Search Algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,49 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - performing jump search algorithm
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t jump, prev, curr, i;
+
+	if (array == NULL || size == 0)
+	{
+		return (-1);
+	}
+
+	jump = (size_t)sqrt(size);
+	prev = 0;
+
+	while (array[prev] < value)
+	{
+		curr = prev + jump;
+
+		if (curr >= size)
+		{
+			curr = size -1;
+
+		}
+
+		printf("Value checked array[%lu] = [%d]\n", curr, array[curr]);
+
+		if (array[curr] >= value || curr == size - 1)
+		{
+			break;
+		}
+		prev += jump;
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", prev, curr);
+
+	for (i = prev; i <= curr && i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
